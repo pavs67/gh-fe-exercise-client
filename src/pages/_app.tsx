aced643655182fdb95d3e4ec1ff9b3d17091c071
Pav <pav@workchain.co.uk>
2024-10-05 12:08:12 +0100

    updated initial pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/app.scss";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import ProductCard from "~/components/Product/ProductCard";
+import Header from "~/components/Header/Header";
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-	require('../mocks')
-}
+// if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+// 	require('../mocks')
+// }
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter", // Define the custom CSS variable for Inter font
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+  return (
+    <div className={`${inter.className} ${inter.variable}`}>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
