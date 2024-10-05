5c9dd5eb4012f646923903a6bd53f0287f1dcc9f
Pav <pav@workchain.co.uk>
2024-10-05 18:29:42 +0100

    updated header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import React, { useContext } from "react";
 import { FC } from "react";
+import { CartContext } from "~/context/cart-context";
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
-  return (
-    <header className="header">
-      <div className="container">
-        <div className="header__inner">
-          <Link href="/">Logo</Link>
+  const { cartItems } = useContext(CartContext);
+  console.log("🚀 ~ cartItems:", cartItems);
 
-          <div className="header__cart"></div>
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header__inner">
+            <Link href="/">
+              Cosmetic Company Logo
+              {/* <img src="cosmetic-logo.png" alt="Cosmetic logo" width={100} /> */}
+            </Link>
+
+            <div className="header__cart">{cartItems.length > 0 && cartItems.length}</div>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <div className="header-spacer"></div>
+    </>
   );
 };
 
